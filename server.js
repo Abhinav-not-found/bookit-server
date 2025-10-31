ce479e006e9aaaedffee8d8b73cadbe4067c5443
abhinav <abhinavisgreat18@gmail.com>
2025-10-31 22:39:15 +0530

    updated allowedOrigins

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ import experienceRoute from './routes/experience.route.js'
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://bookit-client-five.vercel.app/',
-  'https://bookit-client-qnr5e8r42-abhinavnotfounds-projects.vercel.app/'
+  'https://bookit-client-five.vercel.app',
+  'https://bookit-client-qnr5e8r42-abhinavnotfounds-projects.vercel.app'
 ];
 
 
